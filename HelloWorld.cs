d63ea44e0180f632017bdec0d6ed4518ace832b3
c4leb-git <92014022+c4leb-git@users.noreply.github.com>
2021-10-07 20:37:09 -1000

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+*/
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+
+using System;
+public static class Kata
+{
+  public static string greet()
+    {
+        return "hello world!";    
+    }
+}
