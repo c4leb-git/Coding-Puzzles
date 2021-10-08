516f95064890a83a807a7a9016fa535a1786d24a
c4leb-git <92014022+c4leb-git@users.noreply.github.com>
2021-10-07 20:35:02 -1000

    Create ConvertABooleanToString.cs

---
+++ ConvertABooleanToString.cs
@@ -0,0 +1,13 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+
+using System;
+
+public class kata
+{
+  public static string boolean_to_string(bool b) => Convert.ToString(b);
+}
